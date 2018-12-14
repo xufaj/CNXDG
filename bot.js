ea2f360812c86072a5bf19fb80f7fc9b98d38e01
xufaj <45463607+xufaj@users.noreply.github.com>
2018-12-14 14:54:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -163,7 +163,45 @@ client.user.setStatus("dnd")
 }).catch(console.error)
 })
 
-
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("اي دي سيرفرك - Server id");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+ 
+ 
+ 
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("اي دي الروم - Room id");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("اي دي سيرفرك - Server id");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;        
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
 
 	
 	
